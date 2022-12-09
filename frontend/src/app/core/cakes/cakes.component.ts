f138ed964dc3dc125f558c836539bc41095382dd
Vlado Ivkovic <ivkovicv90@gmail.com>
2022-12-09 18:38:25 +0100

    Backend deleted,Cakes component done

--- frontend/src/app/core/cakes/cakes.component.ts
+++ frontend/src/app/core/cakes/cakes.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Cake } from 'src/app/model/cake';
+import { CakesService } from 'src/app/service/cakes.service';
 
 @Component({
   selector: 'app-cakes',
   templateUrl: './cakes.component.html',
-  styleUrls: ['./cakes.component.css']
+  styleUrls: ['./cakes.component.css'],
 })
 export class CakesComponent implements OnInit {
+  cakes: Cake[] = [];
+  ingredients: string[] = [];
 
-  constructor() { }
+  params = {
+    sort: 'name',
+    sortDirection: 'asc',
+    filter: {
+      ingredients: '',
+    },
+  };
+
+  constructor(private service: CakesService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.getCakes();
+    this.getIngredients();
   }
 
+  getCakes(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.service.getAllCakes(this.params).subscribe({
+        next: (response: Cake[]) => {
+          this.cakes = response;
+        },
+        error: (response: any) => {
+          console.log('Error: ', response.statusText);
+        },
+      });
+    });
+  }
+  getIngredients(): void {
+    this.service.getAllIngredients().subscribe({
+      next: (response: string[]) => {
+        this.ingredients = response;
+      },
+      error: (response: any) => {
+        console.log('Error: ', response.statusText);
+      },
+    });
+  }
+  filterIngredients(event: any) {
+    this.params.filter.ingredients = event.target.value;
+    this.getCakes();
+  }
 }
--- frontend/src/app/service/cakes.service.ts
+++ frontend/src/app/service/cakes.service.ts
@@ -1,9 +1,12 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
+import { Cake } from '../model/cake';
 import { SlideShow } from '../model/slideshow';
 
 const slideUrl = 'http://localhost:3000/api/slideshow';
+const cakesUrl = 'http://localhost:3000/api/cakes';
+const ingredientsUrl = ' http://localhost:3000/api/ingredients';
 
 @Injectable({
   providedIn: 'root',
@@ -15,6 +18,32 @@ export class CakesService {
     return this.httpClient.get(slideUrl).pipe(
       map((data: any) => {
         return data && data.map((elem: any) => new SlideShow(elem) || []);
+      })
+    );
+  }
+  getAllCakes(params?: any): Observable<Cake[]> {
+    let options = {};
+    if (params) {
+      options = {
+        params: new HttpParams()
+          .set('sort', params.sort || '')
+          .set('sortDirection', params.sortDirection || '')
+          .set(
+            'filter',
+            (params.filter && JSON.stringify(params.filter)) || ''
+          ),
+      };
+    }
+    return this.httpClient.get(cakesUrl, options).pipe(
+      map((data: any) => {
+        return data && data.map((elem: any) => new Cake(elem) || []);
+      })
+    );
+  }
+  getAllIngredients(): Observable<string[]> {
+    return this.httpClient.get(ingredientsUrl).pipe(
+      map((data: any) => {
+        return data as Array<string>;
       })
     );
   }
